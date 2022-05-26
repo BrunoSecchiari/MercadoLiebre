35f41146ccd80585b62649538ba5e228b87aa831
Bruno Secchiari <brunosecchiari95@gmail.com>
2022-05-26 16:43:39 -0300

    First commit of the project.

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+//views 
+const views = path.join(__dirname, 'views/')
+//public
+const public = path.join(__dirname, 'public/')
+const home_html = path.join(__dirname, 'views/home.html')
+const register_html = path.join(__dirname, 'views/register.html')
+const login_html = path.join(__dirname, 'views/login.html')
+//html
+const htpptRaiz = '/'
+const htpptHome = '/home'
+const htpptRegister = '/register'
+const htpptLogin = '/login'
+//html
+const homehtml = 'home.html'
+
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+//Definir los camions
+app.listen(port || process.env.PORT, () => console.log('Example app listening at http://localhost:'+ port))
+
+app.get(htpptRaiz, function(req, res){
+    res.sendFile(home_html)
+})
+app.get(htpptHome, function(req, res){
+    res.sendFile(home_html)
+})
+app.get(htpptRegister, function(req, res){
+    res.sendFile(register_html)
+})
+app.get(htpptLogin, function(req, res){
+    res.sendFile(login_html)
+})
+app.post(htpptLogin, function(req, res){
+    res.sendFile(login_home_html)
+})
+app.post(htpptRegister, function(req, res){
+    res.sendFile(login_html)
+})
